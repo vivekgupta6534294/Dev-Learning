5b4fe7c08ba25164a6b58320fae5390d219e262f
vivek gupta <vivekgupta6534294@gmail.com>
2022-03-08 22:24:28 +0530

    "Objects in javaScript"

---
+++ Module 2/object.js
@@ -0,0 +1,29 @@
+let obj={};
+console.log(obj);
+
+let objVivek={
+    Name:"Vivek",
+    Age:21,
+    "Phone no": "9477423XXX",
+    lastName:'Gupta'
+}
+
+console.log(objVivek);
+let capAmerica={
+    Name:'Steve',
+    Age:9999,
+    Friends:['Natasha','Thor','Tony','Bucky','Bruce'],
+    address:{
+        city:"Queens",
+        state:"Harayana"
+    },
+    sayHi:function(){
+        // address:{
+        //     city:"Queens"
+        //     state:"Harayana"
+        // }
+
+        console.log("Captain America says Hiii");
+    }
+}
+console.log(capAmerica);
