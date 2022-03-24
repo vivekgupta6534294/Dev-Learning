bf33fb2e57fd8b2224d874344a80e9238b6eaa06
vivek gupta <vivekgupta6534294@gmail.com>
2022-03-24 23:04:04 +0530

    "js itvw"

--- Module2/interview/executionContext.js
+++ Module2/interview/executionContext.js
@@ -5,3 +5,25 @@ function fun(){
 }
 var c=20;
 fun();
+
+
+//
+
+// console.log(a);
+
+// var a = 10;
+
+// console.log(a);
+
+// function fun(){
+//     var b = 20;
+//     return b;
+// }
+
+// console.log(c);
+
+// var c = 30;
+
+// var d = fun();
+
+// console.log(d);
