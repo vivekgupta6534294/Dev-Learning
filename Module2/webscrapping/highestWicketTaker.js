639574f21b2f83ab2483c380cc8f5e9c4503bfde
vivek gupta <vivekgupta6534294@gmail.com>
2022-03-29 23:43:39 +0530

    "batsmanDetail done"

--- Module2/webscrapping/highestWicketTaker.js
+++ Module2/webscrapping/highestWicketTaker.js
@@ -11,8 +11,36 @@ function cb(error, response, html){
     if(error){
         console.log(error);
     }else{
-        console.log(html);
+        // console.log(html);
         const dom=new JSDOM(html);
         const document=dom.window.document;
+        let bowlerTable =document.querySelectorAll(".table.bowler");
+        // console.log(bowlerName.length);
+        let HighestWicket =0 ;
+        let NameOfHighestWicketTaker;
+        for(let i=0;i<bowlerTable.length;i++){
+            let rows=bowlerTable[i].querySelectorAll("tbody tr");
+            // console.log(rows.length);
+            
+            for( let j=0;j<rows.length;j++){
+                let tds=rows[j].querySelectorAll("td");
+                
+                if(tds.length>1){
+                    let name=tds[0].textContent;
+                    let wicket=tds[4].textContent;
+                    
+                    if(wicket>HighestWicket){
+                        HighestWicket=wicket;
+                        NameOfHighestWicketTaker=name;
+                    }
+
+                    // console.log("Name of Bolwer --> ", name, "\t Wicket --> ", wicket);
+                }
+            }
+        }
+        console.log("Name of Highest Wicket Taker", NameOfHighestWicketTaker);
+        console.log("Wicket Taken ", HighestWicket);
+
+        
     }
 }
