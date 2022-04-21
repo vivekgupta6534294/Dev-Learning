ef4d6527661188ea5386e44c0b094432aaceb90b
vivek gupta <vivekgupta6534294@gmail.com>
2022-04-21 21:55:54 +0530

    SeriallyPromise

--- Module2/Automation/seriallyPromise.js
+++ Module2/Automation/seriallyPromise.js
@@ -1,13 +1,24 @@
 let fs = require("fs");
-
+let arr=['./f1.txt', './f2.txt','./f3.txt'];
 let f1KaPromise = fs.promises.readFile("f1.txt");
 
+// f1KaPromise.then(function(data){
+//     console.log(data+"");
+//     let f2KaPromise = fs.promises.readFile("f2.txt");
+//     return f2KaPromise;
+// }).then(function(data){
+//     console.log(data+"");
+// }).catch(function(error){
+//     console.log(error);
+// })
+
+for(let i=1;i<arr.length;i++){
+    f1KaPromise=f1KaPromise.then(function(data){
+        console.log(data +" ");
+        let nextFilePromise=fs.promises.readFile(arr[i]);
+        return nextFilePromise;
+    })
+}
 f1KaPromise.then(function(data){
-    console.log(data+"");
-    let f2KaPromise = fs.promises.readFile("f2.txt");
-    return f2KaPromise;
-}).then(function(data){
-    console.log(data+"");
-}).catch(function(error){
-    console.log(error);
+    console.log(data+" ");
 })
