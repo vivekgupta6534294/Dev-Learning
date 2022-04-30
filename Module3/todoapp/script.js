ebc75d354e8dbed390d51f617813d781df85408f
vivek gupta <vivekgupta6534294@gmail.com>
2022-05-01 00:25:41 +0530

    "to do app"

---
+++ Module3/todoapp/script.js
@@ -0,0 +1,30 @@
+let inputTag = document.querySelector("input");
+let ulTag = document.querySelector("ul");
+
+inputTag.addEventListener('keydown',function(e){
+    // console.log(e);
+    let key = e.key;
+    if(key == 'Enter'){
+        let value = inputTag.value;
+        // console.log(value);
+        if(value.length == 0){
+            return;
+        }
+        inputTag.value = ""; //ui input update 
+        let liTag = document.createElement("li");
+        liTag.innerHTML = `<div>${value}</div>
+                            <div class="delete"><i class="fa fa-trash"></i></div>`
+
+        //other way
+        // liTag.innerHTML = "<div>"+value+"</div><div><i class='fa fa-trash'></i></div>"
+        handleRemoval(liTag);
+        ulTag.appendChild(liTag);
+    }
+})
+
+function handleRemoval(liTag){
+    let deleteDiv = liTag.querySelector(".delete"); 
+    deleteDiv.addEventListener("click",function(){
+        liTag.remove();
+    })
+}
