e3833f659dc16f343f849b590c5e838c76b148f3
vivek gupta <vivekgupta6534294@gmail.com>
2022-05-12 23:50:11 +0530

    Jira App  Function Added Along with Ui

--- Module3/JIRA CLONE/script.js
+++ Module3/JIRA CLONE/script.js
@@ -2,7 +2,12 @@ let addBtn = document.querySelector(".add-btn");
 let modalCont = document.querySelector(".modal-cont");
 let taskAreaCont = document.querySelector(".textarea-cont");
 let mainCont = document.querySelector(".main-cont")
+let allPriorityColors = document.querySelectorAll(".priority-color");
+let removeBtn = document.querySelector(".remove-btn");
 let addModal = true;
+let removeFlag = false;
+let colors = ['lightpink','blue','green','black']
+let modalPriorityColor = colors[colors.length-1];
 
 addBtn.addEventListener("click",function(){
     if(addModal){
@@ -15,19 +20,39 @@ addBtn.addEventListener("click",function(){
     addModal = !addModal;
 })
 
+for(let i=0;i<allPriorityColors.length;i++){
+    let priorityDivOneColor = allPriorityColors[i];
+    priorityDivOneColor.addEventListener("click",function(){        
+        for(let j=0;j<allPriorityColors.length;j++){
+            allPriorityColors[j].classList.remove("active");
+        }
+        priorityDivOneColor.classList.add("active");
+        modalPriorityColor = priorityDivOneColor.classList[0];
+    })
+}
+
 modalCont.addEventListener("keydown",function(e){
     let key = e.key;
     if(key == 'Enter'){
-        createTicket(taskAreaCont.value);
+        createTicket(modalPriorityColor, taskAreaCont.value);
         taskAreaCont.value = "";
         modalCont.style.display = "none";
         addModal = !addModal;
     }
 })
 
+removeBtn.addEventListener("click",function(){
+    if(removeFlag){
+        removeBtn.style.color = 'black'
+    }else{
+        removeBtn.style.color = "red"
+    }
+    removeFlag = !removeFlag;
+})
 
 
-function createTicket(task){
+
+function createTicket(ticketColor,task){
     // <div class="ticket-cont">
     //         <div class="ticket-color"></div>
     //         <div class="ticket-id">#qzu03</div>
@@ -35,8 +60,31 @@ function createTicket(task){
     //     </div>
     let ticketCont = document.createElement("div");
     ticketCont.setAttribute('class','ticket-cont');
-    ticketCont.innerHTML = `<div class="ticket-color"></div>
+    ticketCont.innerHTML = `<div class="ticket-color ${ticketColor}"></div>
                             <div class="ticket-id">#qzu03</div>
                             <div class="task-area">${task}</div>`
     mainCont.appendChild(ticketCont);
+
+    //handling delete 
+    ticketCont.addEventListener("click",function(){
+        if(removeFlag)
+            ticketCont.remove();
+    })
+
+    //handle color
+    let ticketColorBand = ticketCont.querySelector(".ticket-color");
+    ticketColorBand.addEventListener("click",function(){
+        let currentTicketColor = ticketColorBand.classList[1];
+        let currentTicketColorIdx = -1;
+        for(let i=0;i<colors.length;i++){
+            if(currentTicketColor == colors[i]){
+                currentTicketColorIdx = i;
+                break;
+            }
+        }
+        let nextColorIdx = (currentTicketColorIdx+1)%colors.length;
+        let nextColor = colors[nextColorIdx];
+        ticketColorBand.classList.remove(currentTicketColor);
+        ticketColorBand.classList.add(nextColor);
+    })
 }
