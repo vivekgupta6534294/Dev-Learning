89bd435fd921e5f18c85a67bed85e3679b851e9b
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-07 01:52:37 +0530

    handleChange handle AddTask

--- Module 4/react/my-app/src/components/Todo.js
+++ Module 4/react/my-app/src/components/Todo.js
@@ -1,35 +1,45 @@
 import { Component } from "react";
 
+
 class Todo extends Component{
     constructor(){
         super();
-        this.state={
-            tasks:["Learn Js","Learn DOM","Learn React"]
+        this.state = {
+            tasks:["Learn JS","Learn DOM","Learn React","Learn Node"],
+            currTask:""
         }
     }
+    
+    handleChange = (e)=>{
+        // console.log(e.target.value);
+        this.setState({
+            currTask:e.target.value
+        })
+    }
+
+    handleAddTask = ()=>{
+        this.setState({
+            tasks:[...this.state.tasks,this.state.currTask],
+            currTask:""
+        })
+    }
+
     render(){
         return(
             <div>
-                <input type="text"></input>
-                <button>Add Task</button>
+                <input type="text" value={this.state.currTask} onChange={this.handleChange}/>
+                <button onClick={this.handleAddTask}>Add Task</button>
                 <ul>
-                    {/* <li>
-                        Task 1
-                    </li>
-                    <li>
-                        Task 2
-                    </li> */}
                     {this.state.tasks.map((ele)=>(
                         <li>
                             <p>{ele}</p>
+                            <button>Delete</button>
                         </li>
-
                     ))}
                 </ul>
-
             </div>
-           
         )
     }
 }
+
 export default Todo;
