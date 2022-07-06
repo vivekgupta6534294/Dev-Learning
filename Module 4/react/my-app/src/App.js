e5f24331926be185c776341a6f32d7bfc58f65c0
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-07 01:17:08 +0530

    "react npx"

---
+++ Module 4/react/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './components/Todo.js'
+
+function App() {
+  return (
+   <h1>
+    <Todo/>
+   </h1>
+  );
+}
+
+export default App;
---
+++ Module 4/react/my-app/src/components/Todo.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class Todo extends Component{
+    render(){
+        return(
+            <div>This is Todo Component</div>
+        )
+    }
+}
+export default Todo;
