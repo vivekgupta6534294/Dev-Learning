df803e7b9a1476b5b882fe8cb608d153bbdd8d76
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-07 01:31:14 +0530

    static list show

--- Module 4/react/my-app/src/components/Todo.js
+++ Module 4/react/my-app/src/components/Todo.js
@@ -1,9 +1,34 @@
 import { Component } from "react";
 
 class Todo extends Component{
+    constructor(){
+        super();
+        this.state={
+            tasks:["Learn Js","Learn DOM","Learn React"]
+        }
+    }
     render(){
         return(
-            <div>This is Todo Component</div>
+            <div>
+                <input type="text"></input>
+                <button>Add Task</button>
+                <ul>
+                    {/* <li>
+                        Task 1
+                    </li>
+                    <li>
+                        Task 2
+                    </li> */}
+                    {this.state.tasks.map((ele)=>(
+                        <li>
+                            <p>{ele}</p>
+                        </li>
+
+                    ))}
+                </ul>
+
+            </div>
+           
         )
     }
 }
