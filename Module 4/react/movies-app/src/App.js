c3ac3614cd59c99ddc8633da4dac6f0411a26b43
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-08 01:08:52 +0530

    MovieList component added

--- Module 4/react/movies-app/src/App.js
+++ Module 4/react/movies-app/src/App.js
@@ -1,15 +1,16 @@
-
 import './App.css';
-import NavBar from './components/NavBar.js'
-import Banner from './components/Banner.js'
+import Banner from './components/Banner';
+import MovieList from './components/MovieList';
+import NavBar from './components/NavBar';
+
 function App() {
   return (
-    <div>
-    <NavBar/>
-    <Banner/>
-    </div>
-
-  );
+    <>
+      <NavBar/>
+      <Banner/>
+      <MovieList/>
+    </>
+    );
 }
 
 export default App;
