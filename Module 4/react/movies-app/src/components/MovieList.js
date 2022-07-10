b6bb78632b9eb772ebecfe1e5853a421d32d2143
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-11 00:01:55 +0530

    Local Storage and Favourites

--- Module 4/react/movies-app/src/components/MovieList.js
+++ Module 4/react/movies-app/src/components/MovieList.js
@@ -9,7 +9,8 @@ class MovieList extends Component {
             hover:"",
             pArr :[1],
             movies:[],
-            currPage:1
+            currPage:1,
+            favourites:[]
         };
     }
 
@@ -31,24 +32,46 @@ class MovieList extends Component {
     }
 
     handleNext=()=>{
-        this.setState({
+        this.setState({ 
             pArr:[...this.state.pArr,this.state.pArr.length+1],
             currPage:this.state.currPage+1
         },this.changeMovies)
     }
-    handlePrevious=()=>{
-        if(this.state.currPage!=1)
+
+    handlePrev=()=>{
+        if(this.state.currPage!=1){
             this.setState({
-                    currPage:this.state.currPage-1
+                currPage:this.state.currPage-1
             },this.changeMovies);
-    }  
+        }
+    }
+
     handlePageClick=(ele)=>{
         if(ele!=this.state.currPage){
             this.setState({
-                currPage:ele
-            },this.changeMovies)
+                currPage: ele
+            },this.changeMovies);
         }
+    }
+
+    handleFavourites = (movieObj)=>{
+        let oldData = JSON.parse(localStorage.getItem('movies-app') || '[]')
+        if(this.state.favourites.includes(movieObj.id)){
+            oldData = oldData.filter((movie)=>movie.id!=movieObj.id)
+        }else{
+            oldData.push(movieObj)
         }
+        localStorage.setItem("movies-app",JSON.stringify(oldData));
+        this.handleFavouritesState();
+    }
+
+    handleFavouritesState = ()=>{
+        let oldData = JSON.parse(localStorage.getItem('movies-app')|| '[]')
+        let temp = oldData.map((movie)=>movie.id);
+        this.setState({
+            favourites:[...temp]
+        })
+    }
 
     render() {
         console.log("rednered");
@@ -65,7 +88,9 @@ class MovieList extends Component {
                             <h5 className="card-title movie-title">{movieEle.title}</h5>
                             <div style={{ display: 'flex', justifyContent: "center" }}>
                                 {this.state.hover == movieEle.id && (
-                                <a href="#" type="button" className="btn btn-primary movies-button">Add to Favourites</a>)}
+                                <a type="button" className="btn btn-primary movies-button" onClick={()=>this.handleFavourites(movieEle)}>
+                                    {this.state.favourites.includes(movieEle.id)?"Remove from Favourites":"Add to Favourites"}
+                                </a>)}
                             </div>
                         </div>
                     ))}
@@ -73,7 +98,7 @@ class MovieList extends Component {
                 <div style={{display:"flex",justifyContent:"center"}}>
                 <nav aria-label="Page navigation example">
                     <ul className="pagination">
-                        <li className="page-item"><a className="page-link" onClick={this.handlePrevious}>Previous</a></li>
+                        <li className="page-item"><a className="page-link" onClick={this.handlePrev}>Previous</a></li>
                         {this.state.pArr.map((ele)=>(
                             <li className="page-item"><a className="page-link" onClick={()=>this.handlePageClick(ele)}>{ele}</a></li>
                         ))}
