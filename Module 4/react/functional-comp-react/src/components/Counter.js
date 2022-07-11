3dac791bf5326689a85ac86bc0ac9583630942ee
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-12 01:42:17 +0530

    Functional Component

---
+++ Module 4/react/functional-comp-react/src/components/Counter.js
@@ -0,0 +1,14 @@
+import {useState} from "react"
+function Counter(){
+    const[count, setCount]=useState(0);
+    return (
+        <div>
+            <h1>
+                This is Counter {count}
+            </h1>
+            <button onClick={()=>setCount(count+1)}>+</button>
+            <button onClick={()=>setCount(count-1)}>-</button>
+        </div>
+    )
+}
+export default Counter
