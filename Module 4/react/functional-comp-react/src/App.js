947d1fcafb3acb70e3145d92862c26f3925d5ccf
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-12 22:52:21 +0530

    Context Api Theme Changer

--- Module 4/react/functional-comp-react/src/App.js
+++ Module 4/react/functional-comp-react/src/App.js
@@ -5,12 +5,14 @@ import Ue1 from './components/Ue1';
 import Ue2 from './components/Ue2';
 import Ue3 from './components/Ue3';
 import Parent from './components/Context/Parent';
+import ThemeChanger from './components/Context/ThemeChanger'
 
 function App() {
   return (
     // <Counter/>
     // <Ue3/>
-    <Parent/>
+    // <Parent/>
+    <ThemeChanger/>
   );
 }
 
