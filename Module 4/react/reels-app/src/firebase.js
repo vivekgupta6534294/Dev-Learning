862e238328f99bdc5e960c33102e5bf6e20acced
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-16 22:06:47 +0530

    Login setup

---
+++ Module 4/react/reels-app/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMVMWvzFWYMwW0OLhc5hkVQc-cdr1vats",
+  authDomain: "class-demo-fb102.firebaseapp.com",
+  projectId: "class-demo-fb102",
+  storageBucket: "class-demo-fb102.appspot.com",
+  messagingSenderId: "666091823385",
+  appId: "1:666091823385:web:93f6681a7fbb430d35a588"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
