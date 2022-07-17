c3aaf046a1283bdf200b12317208cc35ef6d37b9
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-17 22:32:08 +0530

    hide your api

--- Module 4/react/reels-app/src/firebase.js
+++ Module 4/react/reels-app/src/firebase.js
@@ -2,21 +2,15 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 import {getFirestore} from "firebase/firestore"
+import secret from "./secret"
  // Import krna hoga to use
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCMVMWvzFWYMwW0OLhc5hkVQc-cdr1vats",
-  authDomain: "class-demo-fb102.firebaseapp.com",
-  projectId: "class-demo-fb102",
-  storageBucket: "class-demo-fb102.appspot.com",
-  messagingSenderId: "666091823385",
-  appId: "1:666091823385:web:93f6681a7fbb430d35a588"
-};
+
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(secret);
 export let auth = getAuth(app);// Export krna hoga
 export const db=getFirestore(app);
