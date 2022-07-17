a8b7a2e5ce392de54fe9abc240137bdd7d8aefde
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-17 22:58:57 +0530

    Profile CSS

--- Module 4/react/reels-app/src/components/Profile.jsx
+++ Module 4/react/reels-app/src/components/Profile.jsx
@@ -1,10 +1,20 @@
-
+import "./profile.css"
 
 function Profile(){
     return (
-        <div>
-            Profile
+        <>
+        <div className="header"></div>
+        <div className="main">
+            <div className="pimg_container">
+                <img src="https://cdn4.iconfinder.com/data/icons/teamwork-v2/64/user_person_people_persona_staff_student_man-512.png" alt="" className="pimg" />
+            </div>
+            <div className="details">
+                <div className="content">Vivek</div>
+                <div className="content">No of Posts: <span className="bold_text">Posts</span></div>
+                <div className="content">Email <span className="bold_text">vivekgupta6534294@gmail.com</span></div>
+            </div>
         </div>
+        </>
     )
 }
 
